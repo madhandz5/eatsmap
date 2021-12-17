d958de1cfbe9e2262cae53c195bf160c3f93c6c3
Ryan Oh <madhandz5@gmail.com>
2021-12-17 11:08:43 +0900

    first commit

---
+++ src/main/java/com/eatsmap/EatsmapApplication.java
@@ -0,0 +1,13 @@
+package com.eatsmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatsmapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EatsmapApplication.class, args);
+    }
+
+}
